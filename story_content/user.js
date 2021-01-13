936a6c5f68e6a155533347bc3487523350061445
Mariskt <mariannes@skillup.tech>
2021-01-13 14:49:28 -0800

    Addfiles
    Add project

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6LiXyLSvZ3d":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+alert("Hello!");
+}
+
